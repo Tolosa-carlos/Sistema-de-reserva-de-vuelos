db86d04a59d5107cff154fa0ddc759c78551477e
Carlos Tolosa <carloswtolosa@gmail.com>
2025-06-11 20:14:11 -0300

    Inicio clase Usuario

---
+++ Usuario.cpp
@@ -0,0 +1,38 @@
+//
+// Created by carlo on 11/6/2025.
+//
+
+#include "Usuario.h"
+#include <iostream>
+using namespace std;
+
+Usuario::Usuario() : idUsuario(0), nombre(""), mail(""){}
+Usuario::Usuario(int id, string &nombre, string &mail) : idUsuario(id), nombre(nombre), mail(mail){}
+
+int Usuario::getId() const { return idUsuario; }
+
+string Usuario::getNombre() const { return nombre; }
+void Usuario::setNombre(string &nombre) { this->nombre = nombre; }
+
+string Usuario::getApellido() { return apellido; }
+void Usuario::setApellido(string &apellido) { this->apellido = apellido; }
+
+string Usuario::getMail() const { return mail; }
+void Usuario::setMail(string &mail) { this->mail = mail; }
+
+void Usuario::mostrarDatosUsuario() {
+    cout << "Id: " << idUsuario << endl;
+    cout << "Nombre: " << nombre << endl;
+    cout << "Apellido: " << apellido << endl;
+    cout << "Mail: " << mail << endl;
+}
+
+void Usuario::consultarReservas() const {
+
+}
+
+
+
+
+
+
---
+++ Usuario.h
@@ -0,0 +1,35 @@
+//
+// Created by carlo on 11/6/2025.
+//
+
+#ifndef USUARIO_H
+#define USUARIO_H
+#include <string>
+using namespace std;
+
+
+class Usuario {
+private:
+    int idUsuario;
+    string nombre;
+    string apellido;
+    string mail;
+public:
+    Usuario();
+    Usuario(int id, string &nombre, string &mail);
+
+    int getId() const;
+    string getNombre() const;
+    void setNombre(string &nombre);
+    string getApellido();
+    void setApellido(string &apellido);
+    string getMail() const;
+    void setMail(string &mail);
+    void mostrarDatosUsuario();
+    void consultarReservas() const;
+
+};
+
+
+
+#endif //USUARIO_H
