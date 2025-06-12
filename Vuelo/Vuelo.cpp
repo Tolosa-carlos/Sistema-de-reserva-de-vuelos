c0501aaff6eb1d9689788d39d2ac8a0b0778e4d2
Carlos Tolosa <carloswtolosa@gmail.com>
2025-06-11 21:11:16 -0300

    Avance de la clase Vuelo

--- Vuelo/Vuelo.cpp
+++ Vuelo/Vuelo.cpp
@@ -3,3 +3,53 @@
 //
 
 #include "Vuelo.h"
+#include <iostream>
+using namespace std;
+
+Vuelo::Vuelo() : numVuelo(0), aerolinea(""), origenVuelo(""), destinoVuelo(""), fechaLlegada(), fechaSalida(){}
+Vuelo::Vuelo(int numVuelo, string &aerolinea, string &origen, string &destino, Fecha salida, Fecha llegada) {
+    this->numVuelo = numVuelo;
+    this->aerolinea = aerolinea;
+    origenVuelo = origen;
+    destinoVuelo = destino;
+    fechaSalida = salida;
+    fechaLlegada = llegada;
+}
+
+int Vuelo::getNumVuelo() const { return numVuelo; }
+
+string Vuelo::getAerolinea() const { return aerolinea; }
+void Vuelo::setAerolinea(string &aerolinea) { this->aerolinea = aerolinea; }
+
+string Vuelo::getOrigenVuelo() const { return origenVuelo; }
+void Vuelo::setOrigenVuelo(string &origen) { origenVuelo = origen; }
+
+string Vuelo::getDestinoVuelo() const { return destinoVuelo; }
+void Vuelo::setDestinoVuelo(string &destino) { destinoVuelo = destino; }
+
+Fecha Vuelo::getFechaSalida() const { return fechaSalida; }
+Fecha Vuelo::setFechaSalida(Fecha salida) { fechaSalida = salida; }
+
+Fecha Vuelo::getFechaLlegada() const { return fechaLlegada; }
+Fecha Vuelo::setFechaLlegada(Fecha llegada) { fechaLlegada = llegada; }
+
+void Vuelo::mostrarVuelo() const {
+    cout << "---------------- Datos del vuelo ----------------" << endl;
+    cout << "Número de vuelo: " << numVuelo << endl;
+    cout << "Aerolinea: " << aerolinea << endl;
+    cout << "Origen: " << origenVuelo << endl;
+    cout << "Destino: " << destinoVuelo << endl;
+    cout << "Fecha de salida: "; fechaSalida.mostrarFecha() << endl;
+    cout << "Fecha de llegada: ", fechaLlegada.mostrarFecha() << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
--- Vuelo/Vuelo.h
+++ Vuelo/Vuelo.h
@@ -15,7 +15,24 @@ private:
     string aerolinea;
     string origenVuelo;
     string destinoVuelo;
-    Fecha fecha;
+    Fecha fechaSalida;
+    Fecha fechaLlegada;
+    //Asiento asiento;
+public:
+    Vuelo();
+    Vuelo(int numVuelo, string &aerolinea, string &origen, string &destino, Fecha salida, Fecha llegada);
+    int getNumVuelo() const;
+    string getAerolinea() const;
+    void setAerolinea(string &aerolinea);
+    string getOrigenVuelo() const;
+    void setOrigenVuelo(string &origen);
+    string getDestinoVuelo() const;
+    void setDestinoVuelo(string &destino);
+    Fecha getFechaSalida() const;
+    Fecha setFechaSalida(Fecha salida);
+    Fecha getFechaLlegada() const;
+    Fecha setFechaLlegada(Fecha llegada);
+    void mostrarVuelo() const;
 };
 
 
